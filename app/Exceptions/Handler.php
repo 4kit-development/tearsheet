68891722cf4985adf996696a5839ef312f4f82c3
Jackson Nightingale <jackson@sharedspace.co.nz>
2019-10-08 16:32:16 +1100

    Deployment test

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,7 +48,7 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         if ( $exception instanceof MissingShopDomainException) {
-            abort(403);
+            abort(404);
         }
 
         return parent::render($request, $exception);
